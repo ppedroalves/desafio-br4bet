b8ae1dd1f55ed837b8117011f8ae556f83cc25e4
Pedro Antonio <pedroantonioalvesdeoliveira1@gmail.com>
2023-09-06 12:50:18 -0300

    feat: add all new routes

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CepController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [HomeController::class, 'index']);
+    Route::get('/consulta-cep', [CepController::class, 'index']);
+    Route::post('/consulta-cep', [CepController::class, 'buscarCEP']);
+    Route::get('/profile', [UserController::class, 'profile']);
+    Route::put('/edit-profile', [UserController::class, 'update']);
+    
 });
 
-Route::get('/consulta-cep', [CepController::class, 'index']);
-Route::post('/consulta-cep', [CepController::class, 'buscarCEP']);
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+});
 
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
